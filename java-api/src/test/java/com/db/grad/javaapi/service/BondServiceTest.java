e55036b4dc08e49cfef1fdca4494569067811c12
ancamariac <ciuciuancamaria@gmail.com>
2023-08-07 14:26:14 +0300

    Init get bonds by email test

--- java-api/src/test/java/com/db/grad/javaapi/service/BondServiceTest.java
+++ java-api/src/test/java/com/db/grad/javaapi/service/BondServiceTest.java
@@ -1,17 +1,25 @@
 package com.db.grad.javaapi.service;
 
 import com.db.grad.javaapi.model.Bond;
+import com.db.grad.javaapi.model.Book;
+import com.db.grad.javaapi.model.Trade;
+import com.db.grad.javaapi.model.User;
 import com.db.grad.javaapi.repository.BondsRepository;
+import com.db.grad.javaapi.repository.BooksRepository;
+import com.db.grad.javaapi.repository.TradesRepository;
+import com.db.grad.javaapi.repository.UsersRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.text.ParseException;
 import java.util.*;
@@ -27,9 +35,21 @@ public class BondServiceTest {
     @Mock
     private BondsRepository bondsRepository;
 
+    @Mock
+    private UsersRepository usersRepository;
+
+    @Mock
+    private TradesRepository tradesRepository;
+
+    @Mock
+    private BooksRepository booksRepository;
+
+    @Mock
+    private TradeServiceImpl tradeService;
 
     @Test
-    public void getBondsDueForMaturityPeriod() throws ParseException {
+    public void getBondsDueForMaturityPeriodByEmail() throws ParseException {
+
         Bond bond = new Bond();
         bond.setIsin("ISIN1");
         bond.setType("CORP");
@@ -84,6 +104,104 @@ public class BondServiceTest {
 
         Mockito.when(bondsRepository.findAll()).thenReturn(Arrays.asList(bond, bond2, bond3, bond4));
 
+        // add users
+        User user1 = new User();
+        user1.setEmail("anca@gmail.com");
+        user1.setPassword("12345");
+
+        User user2 = new User();
+        user2.setEmail("maria@gmail.com");
+        user2.setPassword("12345");
+
+        // create books
+        Book book1 = new Book();
+        book1.setId(1);
+        book1.setBookName("book1");
+        Set<User> listOfUsers = new HashSet<>();
+        listOfUsers.add(user1);
+        book1.setUsers(listOfUsers);
+        Set<Book> user1Books = new HashSet<>();
+        user1Books.add(book1);
+        user1.setBooks(user1Books);
+
+        Book book2 = new Book();
+        book2.setId(2);
+        book2.setBookName("book2");
+        Set<User> listOfUsers1 = new HashSet<>();
+        listOfUsers1.add(user2);
+        book2.setUsers(listOfUsers1);
+        Set<Book> user2Books = new HashSet<>();
+        user2Books.add(book2);
+        user2.setBooks(user2Books);
+
+
+        // add trades
+
+        Date date1 = new GregorianCalendar(2023, Calendar.AUGUST, 11).getTime();
+
+        Trade trade1 = new Trade();
+        trade1.setId(3);
+        trade1.setIsin("ABCD");
+        trade1.setBookId(1);
+        trade1.setBondHolderID(1);
+        trade1.setStatus("open");
+        trade1.setUnitPrice(100);
+        trade1.setTradeCurrency("USD");
+        trade1.setTradeType("buy");
+        trade1.setTradeDate(date1);
+        trade1.setTradeSettlementDate(date1);
+        trade1.setQuantity(45);
+        Mockito.when(tradesRepository.save(trade1)).thenReturn(trade1);
+        tradesRepository.save(trade1);
+
+        Trade trade2 = new Trade();
+        trade2.setId(4);
+        trade2.setIsin("ABCD");
+        trade2.setBookId(1);
+        trade2.setBondHolderID(1);
+        trade2.setStatus("open");
+        trade2.setUnitPrice(150);
+        trade2.setTradeCurrency("GBP");
+        trade2.setTradeType("sell");
+        trade2.setTradeDate(date1);
+        trade2.setTradeSettlementDate(date1);
+        trade2.setQuantity(60);
+        Mockito.when(tradesRepository.save(trade2)).thenReturn(trade2);
+        tradesRepository.save(trade2);
+
+        List <Trade> abcdTrades = new ArrayList<>();
+        abcdTrades.add(trade1);
+        abcdTrades.add(trade2);
+
+        Trade trade3 = new Trade();
+        trade3.setId(5);
+        trade3.setIsin("MNOP");
+        trade3.setBookId(2);
+        trade3.setBondHolderID(2);
+        trade3.setStatus("open");
+        trade3.setUnitPrice(110);
+        trade3.setTradeCurrency("GBP");
+        trade3.setTradeType("sell");
+        trade3.setTradeDate(date1);
+        trade3.setTradeSettlementDate(date1);
+        trade3.setQuantity(20);
+        Mockito.when(tradesRepository.save(trade3)).thenReturn(trade3);
+        tradesRepository.save(trade3);
+
+        List <Trade> mnopTrades = new ArrayList<>();
+        mnopTrades.add(trade3);
+
+        TradeServiceImpl tradeServiceMock = Mockito.mock(TradeServiceImpl.class);
+        ReflectionTestUtils.setField(bondService, "tradeService", tradeServiceMock);
+
+        Mockito.when(tradesRepository.findAll()).thenReturn(Arrays.asList(trade1, trade2, trade3));
+
+        Mockito.when(tradeService.getAllTradesByISIN("MNOP")).thenReturn(Arrays.asList(trade3));
+        Mockito.when(tradeService.getAllTradesByISIN("ABCD")).thenReturn(Arrays.asList(trade1, trade2));
+
+        Mockito.when(booksRepository.getReferenceById(1)).thenReturn(book1);
+        Mockito.when(booksRepository.getReferenceById(2)).thenReturn(book2);
+
         Map<String, Map<String, Integer>> expectedMap = new HashMap<>();
         Map<String, Integer> date3Map = new HashMap<>();
         date3Map.put("CORP", 1);
@@ -91,8 +209,11 @@ public class BondServiceTest {
         Map<String, Integer> date4Map = new HashMap<>();
         date4Map.put("GOVN", 1);
         expectedMap.put("11-08-2023", date4Map);
+        for(User user : booksRepository.getReferenceById(trade1.getBookId()).getUsers()){
+            System.out.println(user.getEmail());
+        }
 
-        Map<String, Map<String, Integer>> maturityBonds = bondService.getAllBondsForBusinessDaysBeforeAndAfter("17-08-2023", 5, 5);
+        Map<String, Map<String, Integer>> maturityBonds = bondService.getAllBondsForBusinessDaysBeforeAndAfterOfEmail("17-08-2023", 5, 5, "anca@gmail.com");
         assertEquals(expectedMap, maturityBonds);
     }
 
