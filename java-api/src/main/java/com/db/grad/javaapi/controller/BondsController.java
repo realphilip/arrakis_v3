222b502179b3f893bb46f88411add0974ba3ce12
ancamariac <ciuciuancamaria@gmail.com>
2023-08-07 23:13:55 +0300

    Cleanup code

--- java-api/src/main/java/com/db/grad/javaapi/controller/BondsController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/BondsController.java
@@ -88,34 +88,4 @@ public class BondsController {
         return bondService.getBondByIsin(isin);
     }
 
-
-
-
-//    @PostMapping("/dogs")
-//    public Dog createDog(@Valid @RequestBody Dog dog) {
-//        return dogsService.addDog(dog);
-//    }
-//
-//    @PutMapping("/dogs/{id}")
-//    public ResponseEntity< Dog > updateDog(@PathVariable(value = "id") Long id,
-//                                           @Valid @RequestBody Dog dogDetails) throws ResourceNotFoundException {
-//
-//        final Dog updatedDogs = dogsService.updateDogDetails(dogDetails);
-//        return ResponseEntity.ok(updatedDogs);
-//    }
-//
-//    @DeleteMapping("/dogs/{id}")
-//    public Map< String, Boolean > deleteDog(@PathVariable(value = "id") Long id)
-//            throws ResourceNotFoundException {
-//        boolean removed = dogsService.removeDog(id);
-//
-//        Map < String, Boolean > response = new HashMap<>();
-//        if( removed )
-//            response.put("deleted", Boolean.TRUE);
-//        else
-//            response.put("deleted", Boolean.FALSE);
-//
-//        return response;
-//    }
-
 }
